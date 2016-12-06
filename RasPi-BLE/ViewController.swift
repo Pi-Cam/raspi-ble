388c2bd5004ffe1f9225c2104ce513a87a60035d
Ross Justin <justrostin@gmail.com>
2016-12-05 16:23:20 -0800

    BLE communication with raspi echo app working

--- RasPi-BLE/ViewController.swift
+++ RasPi-BLE/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NRFManagerDelegate {
+  
+  var nrfManager:NRFManager!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    nrfManager = NRFManager(
+      onConnect: {
+        print("C: ★ Connected")
+    },
+      onDisconnect: {
+        print("C: ★ Disconnected")
+    },
+      onData: {
+        (data:Data?, string:String?)->() in
+        print("C: ⬇ Received data - String: \(string) - Data: \(data)")
+    },
+      autoConnect: false
+    )
+    
+    nrfManager.verbose = true
+    nrfManager.delegate = self
   }
+  
+  func sendData() {
+    let string = "Hello World!"
+    let result = self.nrfManager.writeString(string)
+    print("⬆ Sent string: \(string) - Result: \(result)")
+  }
+  
+  @IBAction func connectButtonPressed(_ sender: Any) {
+    nrfManager.connect()
+  }
+  
+  @IBAction func disconnectButtonPressed(_ sender: Any) {
+    nrfManager.disconnect()
+  }
+  
+  @IBAction func sendButtonPressed(_ sender: Any) {
+    sendData()
+  }
+  
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 
 }
 
